4d412060ac72e817adb007a683f4d164e0574b8a
Nguyen Xuan Anh <xuananhnguyen0306@gmail.com>
2025-07-04 00:26:01 +0700

    add Rengtale class

--- LayoutPractice/LayoutPractice/Practice.swift
+++ LayoutPractice/LayoutPractice/Practice.swift
@@ -23,4 +23,16 @@ class Circle: Shape {
     }
 }
 
-
+class Rengtale: Shape {
+    var width: Double
+    var height: Double
+    
+    init(width: Double, height: Double) {
+        self.width = width
+        self.height = height
+    }
+    
+    func area() -> Double {
+        return width * height
+    }
+}
